44f7a5ea08f18940edccfb4ade87b2b7642a54d9
MaansiSaxena <110008099+MaansiSaxena@users.noreply.github.com>
2022-08-25 17:08:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace day2
+{
+    public struct employee {
+        public int id;
+        public string name;
+        public double salary;
+        public string gender;
+    };
+
+    class Program 
+    { 
+        static void Main (string[] args)
+        {
+            employee[] emp = new employee[3];
+            for (int i=0;i<3;i++)
+        {
+            Console.WriteLine("Enter the employee id");
+            emp[i].id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the employee name");
+            emp[i].name = (Console.ReadLine());
+            Console.WriteLine("Enter the employee salary");
+            emp[i].salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the employee gender");
+            emp[i].gender = Console.ReadLine();
+        }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(emp[i].id + " " + emp[i].name + " " + emp[i].salary + " " + emp[i].gender);
+            }       
+        }
+    }
+
+
+}
---
+++ hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day2
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hey " + name);
+            Console.ReadLine();
+        }
+    }
+}
